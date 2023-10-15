2e7969bfaac2fd8f10f331f595cf6bb358c85faf
Pouya Sadeghzadeh <122441650+MrPouyaSaad@users.noreply.github.com>
2023-10-15 12:38:28 +0330

    Add image path to "const.dart"

---
+++ lib/constants/const.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Color scaffoldBackgroundColor = const Color(0xFF343541);
+Color cardColor = const Color(0xFF444654);
+
+String path = 'assets/';
+String openAiLogo = '{$path}openai_logo.jpg';
+String personImage = '{$path}person.png';
+String chatLogo = '{$path}chat_logo.png';
