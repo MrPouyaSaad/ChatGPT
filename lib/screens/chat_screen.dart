1d7cbfe59026a5a0a8cd248d98ad11cc95329a56
Pouya Sadeghzadeh <122441650+MrPouyaSaad@users.noreply.github.com>
2023-10-15 14:41:04 +0330

    Make Bottom Navigation

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,5 +1,7 @@
 import 'package:chat_gpt/constants/const.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -9,10 +11,61 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  bool _isTyping = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: myAppBar(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          if (_isTyping) ...[
+            SpinKitThreeBounce(
+              color: Colors.white,
+              size: 24,
+            ),
+          ],
+          SizedBox(height: 16),
+          MyBottomNavigation(),
+        ],
+      ),
+    );
+  }
+}
+
+class MyBottomNavigation extends StatelessWidget {
+  const MyBottomNavigation({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: cardColor,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 12, bottom: 4, top: 4),
+        child: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                style: TextStyle(color: Colors.white),
+                cursorColor: Colors.white,
+                decoration: InputDecoration.collapsed(
+                  hintText: 'How can I help you?',
+                  hintStyle: TextStyle(color: Colors.grey),
+                ),
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.send,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
