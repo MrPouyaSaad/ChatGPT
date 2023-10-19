7d401c7fca2552b29f74c54fc244efb903aa41b9
Pouya Sadeghzadeh <122441650+MrPouyaSaad@users.noreply.github.com>
2023-10-19 21:07:02 +0330

    Create Models Model

---
+++ lib/models/models_model.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Models {
+  String id;
+  String root;
+  int create;
+  Models({
+    required this.id,
+    required this.root,
+    required this.create,
+  });
+  factory Models.fromJson(Map<String, dynamic> json) =>
+      Models(id: json['id'], root: json['root'], create: json['created']);
+}
