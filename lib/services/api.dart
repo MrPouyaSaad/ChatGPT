620afdc89fbdfbe52ccd75d6d2136e231f180b84
Pouya Sadeghzadeh <122441650+MrPouyaSaad@users.noreply.github.com>
2023-10-20 19:54:42 +0330

    Complete Get Models Request

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -1,11 +1,11 @@
 import 'dart:convert';
 import 'dart:io';
-
 import 'package:chat_gpt/constants/api_const.dart';
+import 'package:chat_gpt/models/models_model.dart';
 import 'package:http/http.dart' as http;
 
 class ApiServices {
-  Future<void> getModels() async {
+  Future<List<Models>> getModels() async {
     try {
       var response = await http.get(Uri.parse('$baseUrl/model'),
           headers: {'Authorization': ' Bearer $apiKey'});
@@ -13,8 +13,14 @@ class ApiServices {
       if (jsonResponse == 'error') {
         throw HttpException(jsonResponse['error']['message']);
       }
+      List temp = [];
+      for (var i in jsonResponse['data']) {
+        temp.add(i);
+      }
+      return Models.modelsList(temp);
     } catch (e) {
       print(e);
+      rethrow;
     }
   }
 }
