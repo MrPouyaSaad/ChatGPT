5a97908cd274a12b65e00c9bce32403ee3929e3a
Pouya Sadeghzadeh <122441650+MrPouyaSaad@users.noreply.github.com>
2023-10-30 13:45:35 +0330

    Make Chat Model

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class ChatModel {
+  final String msg;
+  final int index;
+  ChatModel({
+    required this.msg,
+    required this.index,
+  });
+
+  // ChatModel.fromJson(Map<String, dynamic> json) {}
+}
