d30bede9265f712f717411fc0aff2b0c56d80da5
Pouya Sadeghzadeh <122441650+MrPouyaSaad@users.noreply.github.com>
2023-10-30 17:43:58 +0330

    Create Post req (send msg)

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:chat_gpt/constants/api_const.dart';
+import 'package:chat_gpt/models/chat_model.dart';
 import 'package:chat_gpt/models/models_model.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class ApiServices {
@@ -21,6 +23,49 @@ class ApiServices {
     } catch (e) {
       print(e);
       rethrow;
+    }
+  }
+  // Send msg
+
+  static Future<List<ChatModel>> sendMsg(
+      {required String msg, required String model}) async {
+    try {
+      var response = await http.post(
+        Uri.parse('$baseUrl/chat/completions'),
+        headers: {
+          'Authorization': 'Bearer $apiKey',
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode({
+          "model": model,
+          "messages": [
+            {
+              "role": "user",
+              "content": msg,
+            }
+          ],
+          "temperature": 0.7,
+        }),
+      );
+      Map<String, dynamic> jsonResponse = jsonDecode(response.body);
+      if (jsonResponse.containsKey('error')) {
+        throw Exception("error: ${jsonResponse['error']['message']}");
+      }
+      List<ChatModel> chatList = [];
+      if (jsonResponse['choices'].length > 0) {
+        debugPrint(
+            'response: ${jsonResponse['choices'][0]['message']['content']}');
+        chatList = List.generate(
+          jsonResponse['choices'].length,
+          (index) => ChatModel(
+              msg: jsonResponse['choices'][index]['message']['content'],
+              index: 1),
+        );
+      }
+      return chatList;
+    } catch (e) {
+      print(e);
+      throw e;
     }
   }
 }
