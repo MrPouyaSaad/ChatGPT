51e73e67919e421aa368f4e0615682964521c6b3
Pouya Sadeghzadeh <122441650+MrPouyaSaad@users.noreply.github.com>
2023-11-10 13:32:28 +0330

    Create Chat Provider

---
+++ lib/providers/chat_provider.dart
@@ -0,0 +1,24 @@
+import 'package:chat_gpt/models/chat_model.dart';
+import 'package:chat_gpt/services/api.dart';
+import 'package:flutter/material.dart';
+
+class ChatProvider with ChangeNotifier {
+  List<ChatModel> chatList = [];
+  List<ChatModel> get getchatList {
+    return chatList;
+  }
+
+  void addUserMessage({required String msg}) {
+    chatList.add(ChatModel(msg: msg, index: 0));
+    notifyListeners();
+  }
+
+  Future<void> getAnswers({
+    required String msg,
+    required String modelID,
+  }) async {
+    chatList.addAll(
+      await ApiServices.sendMsg(msg: msg, model: modelID),
+    );
+  }
+}
